59b9bedeee9398032e305df86133860dd8a111f3
yuriyfomin17 <fominyura2012@gmail.com>
2020-10-13 18:57:27 +0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {CSSTransition} from "react-transition-group";
+
+
+const iconTime = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-alarm-fill" fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd"
+          d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
+</svg>
+
+const leftIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-left-short"
+                      fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd"
+          d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+</svg>
+
+const rightIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-right" fill="currentColor"
+                       xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd"
+          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+</svg>
+
+const arrowDownIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-bar-down"
+                           fill="currentColor"
+                           xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd"
+          d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
+</svg>
+
+const messageIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chat-dots" fill="currentColor"
+                         xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd"
+          d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+    <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg>
+
+const bellIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-bell" fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z"/>
+    <path fill-rule="evenodd"
+          d="M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+</svg>
+
+const plusIcon = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-plus-circle" fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path fill-rule="evenodd"
+          d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Navbar>
+                <NavItem icon={plusIcon}/>
+                <NavItem icon={bellIcon}/>
+                <NavItem icon={messageIcon}/>
+                <NavItem icon={arrowDownIcon}>
+                    <DropdownMenu/>
+                </NavItem>
+            </Navbar>
+        </div>
+    );
+}
+
+
+function Navbar(props) {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                {props.children}
+            </ul>
+        </nav>
+    );
+}
+
+function NavItem(props) {
+    const [open, setOpen] = useState(false)
+    console.log('open', open)
+    return (
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+                {props.icon}
+            </a>
+            {open && props.children}
+        </li>
+    );
+}
+
+function DropdownMenu() {
+
+
+    const [activeMenu, setActiveMenu] = useState('main')
+
+    function DropdownItem(props) {
+        return (
+            <a href="#" className="menu-item">
+                <span className="icon-button">{props.leftIcon}</span>
+                {props.children}
+                <span className="icon-right">{props.rightIcon}</span>
+            </a>
+        )
+
+    }
+
+    return (
+        <div className="dropdown">
+            <CSSTransition
+                in={activeMenu === 'main'}
+                unmounOnExit timeout={500}
+                classNames="menu-primary"
+            >
+                <div className="menu">
+
+
+                    <DropdownItem>My Profile</DropdownItem>
+                    <DropdownItem>
+                        {leftIcon}
+                        {rightIcon}
+                        Settings
+                    </DropdownItem>
+                </div>
+            </CSSTransition>
+        </div>
+    )
+
 }
 
 export default App;
+
+
